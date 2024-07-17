2b405ce989b43cfc08291be981af9359d88ee39b
Mikonimo <mikonani413@gmail.com>
2024-07-17 14:09:32 -0700

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,36 @@
 #!/usr/bin/python3
-
+"""Distributes an archive to web servers using Fabric3"""
 from fabric.api import env, put, run
 import os
 
-# Define the web servers
 env.hosts = ['54.157.138.77', '100.26.155.31']
+env.user = 'ubuntu'
+env.key_filename = 'id_rsa'
+
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
+    """Deploys the archive to the web servers"""
     if not os.path.exists(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive
+        put(archive_path, '/tmp/')
+
+        # Extract the archive
         archive_name = os.path.basename(archive_path)
-        archive_folder = archive_name.split(".")[0]
-        release_path = f"/data/web_static/releases/{archive_folder}/"
+        name = archive_name.split(".")[0]
+        path = f"/data/web_static/releases/{archive_folder}/"
+        run('mkdir -p {}'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_name, path))
+        run('rm /tmp/{}'.format(archive_name))
 
-        put(archive_path, "/tmp/")
-
-        # Uncompress the archive to the folder
-        run(f"mkdir -p {release_path}")
-        run(f"tar -xzf /tmp/{archive_name} -C {release_path}")
-
-        # Delete the archive from the web server
-        run(f"rm /tmp/{archive_name}")
-
-        # Move contents out of the extracted folder
-        run(f"mv {release_path}web_static/* {release_path}")
-        run(f"rm -rf {release_path}web_static")
-
-        # Delete the symbolic link
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run(f"ln -s {release_path} /data/web_static/current")
+        # Create a symbolic link
+        run('rm -f /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(path))
 
         print("New version deployed!")
         return True
     except Exception as e:
-        print(f"Error: {e}")
+        print("Deployment failed: ", str(e))
         return False
