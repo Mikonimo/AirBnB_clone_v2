2d7a35f02daab8537bae53bdc05f83222357e1d9
Mikonimo <mikonani413@gmail.com>
2024-07-17 18:38:07 +0300

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from web_static folder"""
-from fabric import task, Config
+"""Fabric script that generates a .tgz archive from web_sttic folder"""
+from fabric.api import local
 from datetime import datetime
 import os
 
-from fabric.api import local
 
-
-@task
-def do_pack(c):
+def do_pack():
     """Creates a .tgz archive"""
-    if not os.path.exists("versions"):
+    if not os.path.exists("version"):
         os.makedirs("versions")
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "versions/web_static_{}.tgz".format(timestamp)
 
-    result = c.local("tar -cvzf {} web_static".format(archive_name))
+    # Create the archive
+    result = local("tar -cvzf {} web_static".format(archive_name))
 
-    if result.ok:
+    # Check if the archive was created successfully
+    if result.succeeded:
         return os.path.abspath(archive_name)
     else:
         return None
