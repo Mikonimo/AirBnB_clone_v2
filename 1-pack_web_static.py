562fc1626132a062f0c9c2f16ea64490bb1788af
Mikonimo <mikonani413@gmail.com>
2024-07-17 18:37:00 +0300

    codestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,8 +4,8 @@ from fabric import task, Config
 from datetime import datetime
 import os
 
-# Importing local command from Fabric API
 from fabric.api import local
+
 
 @task
 def do_pack(c):
@@ -16,10 +16,8 @@ def do_pack(c):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "versions/web_static_{}.tgz".format(timestamp)
 
-    # Create the archive
     result = c.local("tar -cvzf {} web_static".format(archive_name))
 
-    # Check if the archive was created successfully
     if result.ok:
         return os.path.abspath(archive_name)
     else:
