4ce6328bffc9ab92e52467d344da140e32e40f70
Mikonimo <mikonani413@gmail.com>
2024-07-19 10:44:52 -0700

    task2 done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,34 +9,38 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Deploys the archive to the web servers"""
+    """Distributes an archive to the web servers."""
     if not os.path.exists(archive_path):
         return False
 
     try:
-        # Upload the archive
-        print("Uploading Archive")
-        put(archive_path, '/tmp/')
-
-        # Extract the archive
+        # Upload the archive to the /tmp/ directory of the web server
         archive_name = os.path.basename(archive_path)
-        name = archive_name.split(".")[0]
-        path = f"/data/web_static/releases/{name}/"
-        print("Creating Directory")
-        run('mkdir -p {}'.format(path))
-        print("Extracting the archive")
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, path))
-        print("Removing the /tmp/archive")
-        run('rm /tmp/{}'.format(archive_name))
+        archive_folder = archive_name.split(".")[0]
+        release_path = f"/data/web_static/releases/{archive_folder}"
 
-        # Create a symbolic link
-        print("Removing the symbolic link")
-        run('rm -f /data/web_static/current')
-        print("Creating the new symbolic link")
-        run('ln -s {} /data/web_static/current'.format(path))
+        put(archive_path, "/tmp/")
+
+        # Uncompress the archive to the folder
+        run(f"mkdir -p {release_path}")
+        run(f"tar -xzf /tmp/{archive_name} -C {release_path}")
+
+        # Delete the archive from the web server
+        run(f"rm /tmp/{archive_name}")
+
+        # Move contents out of the extracted folder
+        run(f"mv {release_path}/web_static/* {release_path}")
+        run(f"rm -rf {release_path}/web_static")
+
+        # Delete the symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run(f"ln -s {release_path} /data/web_static/current")
 
         print("New version deployed!")
         return True
     except Exception as e:
-        print("Deployment failed: ", str(e))
+        print(f"Error: {e}")
         return False
+
