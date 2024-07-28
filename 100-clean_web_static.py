365aea976607283086bce63f93cb7e3d898df19b
Mikonimo <mikonani413@gmail.com>
2024-07-28 18:57:32 +0300

    new

---
+++ 100-clean_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from fabric.api import *
+import os
+
+env.hosts = ['54.157.138.77', '100.26.155.31']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+def do_clean(number=0):
+    """Deletes out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+    """
+    number = int(number)
+
+    if number < 1:
+        number = 1
+
+    # Clean local archives
+    local_archives = sorted(os.listdir("versions"))
+    if len(local_archives) > number:
+        archives_to_delete = local_archives[:-number]
+        for archive in archives_to_delete:
+            local("rm -f versions/{}".format(archive))
+
+    # Clean remote archives
+    with cd("/data/web_static/releases"):
+        remote_archives = run("ls -1t").split()
+        remote_archives = [arc for arc in remote_archives if "web_static_" in arc]
+        if len(remote_archives) > number:
+            archives_to_delete = remote_archives[:-number]
+            for archive in archives_to_delete:
+                run("rm -rf /data/web_static/releases/{}".format(archive))
