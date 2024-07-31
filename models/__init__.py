15ddb803c704bd495431aedb3328f79ef6cfd5b2
Mikonimo <mikonani413@gmail.com>
2024-07-31 23:33:06 +0300

    fs storage

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ storage_type = os.getenv('HBNB_TYPE_STORAGE')
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-elif storage_type == 'file':
+elif storage_type == 'fs':
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 else:
