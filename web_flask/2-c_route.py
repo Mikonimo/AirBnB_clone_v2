af29c8d60a51507f780ee16a8f839869f2ace052
Mikonimo <mikonani413@gmail.com>
2024-07-31 22:01:08 +0300

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,5 +19,11 @@ def d_hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def d_text(text):
+    """Display 'C' then text"""
+    return 'C' + text.replace('_', ' ')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
