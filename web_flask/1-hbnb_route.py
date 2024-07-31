bd8921f1d884db2d80bf10ecd19fcfb7c6a5dde9
Mikonimo <mikonani413@gmail.com>
2024-07-31 21:26:35 +0300

    docs

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,13 @@ app = Flask(__nam__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Prints HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def d_hbnb():
+    """Prints HBNB"""
     return "HBNB"
 
 
