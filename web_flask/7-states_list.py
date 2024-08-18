3c1fec9266e628753e83b88e07f64ad6118df55b
Mikonimo <mikonani413@gmail.com>
2024-08-18 13:33:00 +0300

    stat.corr

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all State objects"""
-    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
