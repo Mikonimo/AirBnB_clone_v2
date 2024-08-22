0e4154d2164b8c6ba36efd57fd48c8942b6c912a
Mikonimo <mikonani413@gmail.com>
2024-08-22 19:40:44 +0300

    ssh

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,37 +2,25 @@
 """Starts a web flask application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """display a HTML page like 6-index.html from static"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Remove the SQLAlchemy session after each request."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """Display the HTML page with filters."""
-    states = sorted(storage.all('State').values(), key=lambda x: x.name)
-    amenities = sorted(storage.all('Amenity').values(), key=lambda x: x.name)
-
-    # For DBStorage, use `cities` relationship
-    # For FileStorage, use `cities` public getter method
-    cities = []
-    if storage.__class__.__name__ == 'DBStorage':
-        for state in states:
-            cities.extend(state.cities)
-        cities = sorted(cities, key=lambda x: x.name)
-    else:
-        for state in states:
-            cities.extend(state.cities)
-        cities = sorted(cities, key=lambda x: x.name)
-
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities, cities=cities)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
